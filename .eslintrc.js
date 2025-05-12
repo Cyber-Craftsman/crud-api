6e25ef725a370f08ae6485d905c87c56c71f009a
Mykhailo Levus <mykhailo_levus@epam.com>
2025-05-13 00:53:31 +0300

    chore: initial project setup with config, scripts and structure

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
